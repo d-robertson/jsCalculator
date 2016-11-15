c15e48fd4b3bc4c702d27968e79ff023567636d0
derek_robertson <dwilliamrobertson@gmail.com>
2016-11-15 14:58:33 -0800

    rewired up my calc

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
 angular.module('calcApp', [])
 .controller('calcCtrl', ['$scope', function($scope){
   $scope.title = 'JScalculator';
-  $scope.previous;
-  $scope.current;
-  $scope.total = '';
-  $scope.entry = '';
+  $scope.total = 0;
+  $scope.entries = '';
+  $scope.opperator = '';
 
   $scope.button = function(val){
-    $scope.entry = $scope.entry += val;
-    console.log($scope.entry);
-  }
-
-  $scope.addition = function(){
-
-  }
-
-  $scope.subtraction = function(){
-
-  }
-
-  $scope.multiplication = function(){
-
-  }
-
-  $scope.division = function(){
-
+    console.log(typeof(val));
+    if(val !== '+' && val !== '-' && val !== '*' && val !== '/'){
+      console.log('inside if: ', val);
+      $scope.entries += val; 
+    }
+    if(val === '+' || val === '-' || val === '*' || val === '/'){
+      if($scope.opperator){
+        $scope.total = eval($scope.entries);
+        $scope.entries = $scope.total;
+        $scope.entries += val;
+        $scope.opperator = val;
+      } else {
+        $scope.entries += val;
+        $scope.opperator = val;
+      }
+    }    
   }
 
   $scope.clearEntry = function(){
-    $scope.entry = '';
+    $scope.opperator = '';
+    $scope.entries = '';
   }
 
   $scope.clearAll = function(){
-    $scope.total = '';
+    $scope.entries = '';
+    $scope.total = 0;
+    $scope.opperator = '';
   }
 
   $scope.equals = function(){
-
+    $scope.total = eval($scope.entries);
+    $scope.entries = $scope.total;
+    $scope.opperator = '';
   }
-
-  $scope.decimal = function(){
-
-  }
-
 }]);
